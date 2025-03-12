f67b495adbe15fb3b3b6f44f280271592d210fd4
Bob Beachill <bbeachill@icloud.com>
2025-03-12 20:39:41 +0000

    Initial Commit

---
+++ Wu-Tang App/Wu_Tang_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wu_Tang_AppApp.swift
+//  Wu-Tang App
+//
+//  Created by Robert Beachill on 12/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct Wu_Tang_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
