7c614a9d653d7342329e1d9e068ae8a851c58ae0
Bob Beachill <bbeachill@icloud.com>
2025-03-12 23:36:54 +0000

    Added basic layout plus assets

--- Wu-Tang App/ContentView.swift
+++ Wu-Tang App/ContentView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var enteredName = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Wu-Tang\nCoder Name Generator")
+                .font(.title)
+                .fontWeight(.black)
+                .multilineTextAlignment(.center)
+                .foregroundStyle(.yellow)
+                .frame(maxWidth: .infinity)
+                .padding(.bottom)
+                .background(.black)
+            
+            Spacer()
+            
+            TextField("Enter name here", text: $enteredName) {
+                //TODO:
+            }
+            .font(.title2)
+            .textFieldStyle(.roundedBorder)
+//            .frame(width: 30)
+            .overlay {
+                RoundedRectangle(cornerRadius: 5)
+                    .stroke(Color.gray, lineWidth: 1)
+            }
+            .padding()
+            
+            Spacer()
+            
+            Image("wu-tang")
+                .resizable()
+                .scaledToFit()
+                
         }
-        .padding()
+//        .ignoresSafeArea(edges: .bottom)
     }
 }
 
